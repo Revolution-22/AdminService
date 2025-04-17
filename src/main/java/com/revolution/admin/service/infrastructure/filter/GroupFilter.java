db47111c84271ff4640d34862976b57eca7bb29d
karolwolny <kwolny31@gmail.com>
2025-04-17 19:29:01 +0200

    Add to lower case

--- src/main/java/com/revolution/admin/service/infrastructure/filter/GroupFilter.java
+++ src/main/java/com/revolution/admin/service/infrastructure/filter/GroupFilter.java
@@ -19,7 +19,7 @@ public class GroupFilter implements Filter {
 
         String groups = httpRequest.getHeader("X-USER-ROLES");
 
-        if (groups != null && !groups.contains("admin")) {
+        if (groups != null && !groups.toLowerCase().contains("admin")) {
             chain.doFilter(request, response);
         } else {
             httpResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "Access forbidden for non admin users.");
